e3065abebd35e4398d37453706b5ce3a6e3e528a
Yelnur300709 <mirasleha8@gmail.com>
2024-10-31 18:00:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+// көбейту!!!!!!!!
+
+// var a = 45;
+// var b = 25;
+
+// var total = a*b;
+
+// console.log(total)
+
+
+
+
+// бөлу!!!!!
+
+// var a = 20;
+// var b = 5;
+
+// var total = a/b;
+
+// console.log(total)
+
+
+
+
+
+// пайыз!!!!!!
+
+// var a = 20;
+// var b = 100;
+
+// var total = a%b;
+
+// console.log(total)
+
+
+
+
+// қосу!!!!
+
+// var a = 2;
+// var b = 1;
+
+// var total = a+b;
+
+// console.log(total)
+
+
+
+// азайту!!!!!
+
+// var a = 10;
+// var b = 5;
+
+// var total = a-b;
+
+// console.log(total)
+
+
+
+// қысқаша жазу азайту
+
+// let x =  5;
+
+// x -= 2;
+
+// console.log(x);
+
+
+
+// қысқаша жазу көбейту
+
+// let x =  5;
+
+// x *= 2;
+
+// console.log(x);
+
+
+
+// қысқаша жазу пайздау
+
+// let x = 10;
+
+// x %= 3;
+
+// console.log(x);
+
+
+
+// қысқаша жазу бөлу
+
+// let x =  5;
+
+// x /= 2;
+
+// console.log(x);
+
+var a = 10;
+
+var b = 5;
+
+var c = 2;
+
+var d = 5;
+
+var e = 10;
+
+ let total = (a + b) * c - d / e;
+
+ console.log(total)
+
+
